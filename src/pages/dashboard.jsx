f6c8079329afc5c63a46086fde43569b788e0da7
Juliet-07 <ohankwerejulietkelechi@gmail.com>
2024-06-03 13:08:30 +0200

    checking git status

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const Dashboard = () => {
   return (
-    <div className=" bg-green-400">
-      <p>Dashoard</p>
+    <div className="w-full grid gap-10 bg-green-400">
+      <div>Summary</div>
+      <div>Order Status</div>
+      <div>Recent Orders</div>
+      <div>Low Stock</div>
     </div>
   );
 };
