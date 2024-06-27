82212126a3289fe619b32f3a6981976de8ed34a5
Timcodes117 <timcodes117@gmail.com>
2024-06-27 16:59:10 -0700

    analytics progress

--- src/pages/analytics.jsx
+++ src/pages/analytics.jsx
@@ -5,12 +5,18 @@ import { IoIosArrowForward } from "react-icons/io";
 import { Link } from "react-router-dom";
 import BarchartComp from "../components/BarchartComp";
 import LinechartComp from "../components/LineChartComp";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import DonutComp from "../components/DonutChartComp";
+import PieChartComp from "../components/PieChartComp";
 
 const Analytics = () => {
   const apiURL = import.meta.env.VITE_REACT_APP_BASE_URL;
   const token = localStorage.getItem("adminToken");
   const [customers, setCustomers] = useState([]);
   const [vendors, setVendors] = useState([]);
+
+  const percentage = 66;
 
   const getCurrentDate = () => {
     const date = new Date();
@@ -117,16 +123,94 @@ const Analytics = () => {
             </Link>
           </div>
         </div>
-     
-        <div className="bg-white rounded-[0.5rem] p-4 flex flex-col border-[1px] border-[gainsboro]">
-          <div className="flex flex-row items-center gap-[10px]">
-            <div className="w-[3.5px] h-[30px] bg-[teal] ml-[-12px] rounded-r-[8px]"></div>
-            <div className="flex flex-row items-center justify-between w-full">
-              <p className="font-bold text-lg">User Engagements</p>
+
+        <div className="bg-white rounded-[0.5rem] p-4 flex  md:h-[300px] flex-col border-[1px] border-[gainsboro]">
+          <div className="flex md:flex-row flex-col flex-1">
+            <div className="flex flex-col flex-[70]">
+              <div className="flex flex-row flex-1 height-[400px] items-center gap-[10px]">
+                <div className="w-[3.5px] h-[30px] bg-[teal] ml-[-12px] rounded-r-[8px]"></div>
+                <div className="flex flex-row items-center justify-between w-full">
+                  <div className="flex flex-col">
+                    <p className="font-bold text-lg">User Engagements</p>
+                    <p className="font-normal text-sm">
+                      Conversion rate: 23.3%
+                    </p>
+                  </div>
+                </div>
+
+              </div>
+              <div className="h-[200px] w-full">
+                <br />
+                <div className="flex flex-row gap-[10px] flex-wrap">
+                  <div className="flex flex-row gap-[10px]">
+                    <div className="w-[60px] h-[45px] rounded-[8px] text-[14px] font-bold text-[green] text-bold flex items-center justify-center bg-green-100">
+                      4.4k
+                    </div>
+                    <div className="flex flex-col">
+                      <p className="text-[16px]">Clicks</p>
+                      <p className="text-[12px] text-grey">
+                        +21.1% vs Last Month
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex flex-row gap-[10px]">
+                    <div className="w-[60px] h-[45px] rounded-[8px] text-[14px] font-bold text-[green] text-bold flex items-center justify-center bg-green-100">
+                      200
+                    </div>
+                    <div className="flex flex-col">
+                      <p className="text-[16px]">Clicks</p>
+                      <p className="text-[12px] text-grey">
+                        +21.1% vs Last Month
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex flex-row gap-[10px]">
+                    <div className="w-[60px] h-[45px] rounded-[8px] text-[14px] font-bold text-[green] text-bold flex items-center justify-center bg-green-100">
+                      6m.33s
+                    </div>
+                    <div className="flex flex-col">
+                      <p className="text-[16px]">Clicks</p>
+                      <p className="text-[12px] text-grey">
+                        +21.1% vs Last Month
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="h-[250px] flex flex-col flex-[30] px-[20px] md:border-l-[1px]">
+              <p>Conversion rate by device</p>
+              <br />
+              <div className="w-full flex flex-row h-[150px] justify-between items-center">
+                <div className="flex flex-col gap-4 items-center justify-center ">
+                  <div className="flex flex-row gap-[15px]">
+                    <div className="w-[30px] h-[30px] rounded-[4px] bg-black" />
+                    <p>Mobile</p>
+                  </div>
+                  <div style={{ width: 66, height: 66 }}>
+                    <CircularProgressbar value={66} text="66%" />
+                  </div>
+                </div>
+                <div className="flex flex-col  items-center justify-evenly">
+                  <div className="h-[50px] w-[1px] bg-black" />
+                  <p>vs</p>
+                  <div className="h-[50px] w-[1px] bg-black" />
+                </div>
+                <div className="flex flex-col gap-4 items-center">
+                  <div className="flex flex-row gap-[15px]">
+                    <div className="w-[30px] h-[30px] rounded-[4px] bg-black" />
+                    <p>Desktop</p>
+                  </div>
+                  <div style={{ width: 66, height: 66 }}>
+                    <CircularProgressbar value={66} text="66%" />
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-          <LinechartComp />
         </div>
+        
         <div className="bg-white rounded-[0.5rem] p-4 flex flex-col border-[1px] border-[gainsboro]">
           <div className="flex flex-row items-center gap-[10px]">
             <div className="w-[3.5px] h-[30px] bg-[teal] ml-[-12px] rounded-r-[8px]"></div>
@@ -136,8 +220,36 @@ const Analytics = () => {
           </div>
           <LinechartComp />
         </div>
-        
-        <div>ROI</div>
+
+        <div className="bg-white rounded-[0.5rem] p-4 flex  md:h-[300px] flex-col border-[1px] border-[gainsboro]">
+          <div className="flex md:flex-row flex-col flex-1">
+            <div className="flex flex-col flex-[50]">
+              <div className="flex flex-row flex-1 height-[400px] items-center gap-[10px]">
+                <div className="w-[3.5px] h-[30px] bg-[teal] ml-[-12px] rounded-r-[8px]"></div>
+                <div className="flex flex-row items-center justify-between w-full">
+                  <div className="flex flex-col">
+                    <p className="font-bold text-lg">Return and Refunds</p>
+                    <p className="font-normal text-sm">
+                      Conversion rate: 23.3%
+                    </p>
+                  </div>
+                </div>
+
+              </div>
+              <div className="h-[200px] w-full">
+                <br />
+                {/* chart here */}
+                <DonutComp />
+              </div>
+            </div>
+
+            <div className="h-[250px] flex flex-col flex-[50] px-[20px] md:border-l-[1px]">
+              <p className="font-bold">Product Performanace</p>
+              <br />
+              <PieChartComp />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
